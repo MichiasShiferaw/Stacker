c8407e3ba6de4bedf4caaab29beaa7093a5e997f
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-01-11 17:55:36 -0500

    Update ProStack.java

--- ProStack.java
+++ ProStack.java
@@ -1,2 +1,12 @@
 //Scan all primitive data types
 import java.util.ArrayList;
+
+
+public class ProStackk<S> {
+  
+  private static final ArrayList<Integer> stacke = new ArrayList<Integer>();
+
+  // Adds integers onto stack
+  public void push(int numbero) {
+    stacke.add(numbero);
+  }
